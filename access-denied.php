59026eea0b640ce90d75c06e7bb030e420c39c5c
Sal Zaydon <zaydons@sunyit.edu>
2015-02-03 18:42:13 -0500

    edited for testing

--- access-denied.php
+++ access-denied.php
@@ -1,8 +1,23 @@
 <?php include('header.php') ?>
-<body>
-<h1>Access Denied </h1>
 <p align="center">&nbsp;</p>
 <h4 align="center" class="err">Access Denied!<br />
-  You do not have access to this page.</h4>
+  Please login to view this page.</h4>
+
+
+  <form id="loginForm" name="loginForm" method="post" action="login-exec.php">
+  <table width="300" border="0" align="center" cellpadding="2" cellspacing="0">
+    <tr>
+      <td width="112"><b>Login</b></td>
+      <td width="188"><input name="login" type="text" class="textfield" id="login" /></td>
+    </tr>
+    <tr>
+      <td><b>Password</b></td>
+      <td><input name="password" type="password" class="textfield" id="password" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="Submit" value="Login" /></td>
+    </tr>
+  </table>
 </body>
 </html>
