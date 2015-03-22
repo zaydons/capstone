7aa139468233e8483ad38f9abd578cc4396eeddc
Sal Zaydon <zaydons@sunyit.edu>
2015-03-22 02:42:51 -0400

    Started adding queries, also new bootstrap theme and navbar

---
+++ nav2.php
@@ -0,0 +1,66 @@
+<div class="bs-component" data-variables="navbar-height,navbar-margin-bottom,navbar-border-radius,navbar-collapse-max-height,navbar-default-bg,
+navbar-default-border,navbar-default-brand-color,navbar-default-brand-hover-color,navbar-default-brand-hover-bg,navbar-default-color,navbar-default-link-color,
+navbar-default-link-hover-color,navbar-default-link-hover-bg,navbar-default-link-active-color,navbar-default-link-active-bg,navbar-default-link-disabled-color,
+navbar-default-link-disabled-bg,navbar-default-toggle-border-color,navbar-default-toggle-hover-bg,navbar-default-toggle-icon-bar-bg,navbar-default-link-active-bg,
+navbar-default-link-active-color">
+<div class="navbar navbar-default">
+<div class="navbar-header">
+  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+    <span class="icon-bar"></span>
+  </button>
+  <a class="navbar-brand" href="member-index.php">TroopSoft</a>
+</div>
+<div class="navbar-collapse collapse navbar-responsive-collapse">
+  <ul class="nav navbar-nav">
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Directories<b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <!-- <li><a href="troop-directory.php">All</a></li> -->
+        <li><a href="scout-directory.php">Scouts</a></li>
+        <li><a href="parent-directory.php">Parents</a></li>
+        <li><a href="adult-directory.php">Leaders</a></li>
+      </ul>
+    </li>
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Advancement<b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="advancement.php">Rank</a></li>
+        <!-- <li><a href="#">Merit Badges</a></li> -->
+        <li><a href="training.php">Adult Training</a></li>
+      </ul>
+    </li>
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Rosters<b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="roster.php">Roster</a></li>
+        <li><a href="member-summary.php">Scout Profile</a></li>
+        <li><a href="patrols.php">Patrols</a></li>
+      </ul>
+    </li>
+    <li><a href="calendar.php">Calendar</a></li>
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Contact<b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="contact-scouts.php">Contact Scouts</a></li>
+        <li><a href="contact-parents.php">Contact Parents</a></li>
+        <li><a href="contact-committee.php">Contact Commitee</a></li>
+        <li><a href="contact-all.php">Contact All</a></li>
+      </ul>
+    </li>
+  </ul>
+   <ul class="nav navbar-nav navbar-right">
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown">Admin <b class="caret"></b></a>
+      <ul class="dropdown-menu">
+        <li><a href="logout.php">Logout</a></li>
+        <li><a href="#">Another action</a></li>
+        <li class="divider"></li>
+        <li><a href="#">Settings</a></li>
+      </ul>
+    </li>
+  </ul>
+</div>
+</div>
+</div>
