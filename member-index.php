887e3500d8df4692249015a9472afc433a3df330
Sal Zaydon <zaydons@sunyit.edu>
2015-04-08 03:06:41 -0400

    Adding stuff to member-index

--- member-index.php
+++ member-index.php
@@ -4,5 +4,14 @@
 <h1>Welcome</h1>
 <?php include('nav2.php') ?>
 
+<p>
+	Welcome to TroopSoft, developed by Sal Zaydon.
+</p>
+<p>
+	I literally do not know what to put on this page, so I am just filling it with random text so it doesn't look so blank.
+</p>
+<p>
+	If you find any bugs please report them to me at zaydons@gmail.com
+</p>
 </body>
 </html>
