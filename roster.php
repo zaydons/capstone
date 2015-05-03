adc49a85e53a32731384e5cea4df0bf60be2427e
Sal Zaydon <zaydons@sunyit.edu>
2015-05-03 19:32:58 -0400

    Add leaders to the roster

--- roster.php
+++ roster.php
@@ -28,8 +28,8 @@ echo "<table class='table table-striped table-hover'>
 while($row = mysqli_fetch_array($result))
 {
 echo "<tr>";
-echo "<td>" . $row['last_name'] . "</td>";
 echo "<td>" . $row['first_name'] . "</td>";
+echo "<td>" . $row['last_name'] . "</td>";
 echo "<td> <a href='parents.php?scout_id=$row[scout_id]'> <button type='button' class='btn btn-primary btn-xs'>View</button></a> </td>"; 
 echo "<td> <a href='emergency-contact.php?scout_id=$row[scout_id]'> <button type='button' class='btn btn-primary btn-xs'>View</button></a> </td>"; 
 echo "</tr>";
@@ -38,6 +38,38 @@ echo "</table>";
 
 mysqli_close($con);
 ?>
+<?php
+$con=mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM leaders");
+echo "<h3> Leaders </h3>";
+echo "<table class='table table-striped table-hover'>
+<tr>
+<th>First Name</th>
+<th>Last Name</th>
+
+
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['firstname'] . "</td>";
+echo "<td>" . $row['lastname'] . "</td>";
+ 
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+?>
+
+
 
 </body>
 </html>
