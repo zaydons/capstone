40bebb56a3add28e5de77a1b5575c9ed5d46acec
Sal Zaydon <sal@zaydon.email>
2017-10-01 23:13:33 -0400

    remove direct links to css

--- header.php
+++ header.php
@@ -6,8 +6,8 @@
 <title>TroopSoft</title>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="http://szaydon.me/troop/bootstrap2/theme.css">
-<link rel="stylesheet/less" href="http://szaydon.me/troop/bootstrap2/theme.less">
-<link rel="stylesheet/less" href="http://szaydon.me/troop/bootstrap2/variable.less">
+<link rel="stylesheet" href="/bootstrap2/theme.css">
+<link rel="stylesheet/less" href="/bootstrap2/theme.less">
+<link rel="stylesheet/less" href="/bootstrap2/variable.less">
 </head>
 
